729997ea022012f7b05e02979492cb50004d2420
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-11-28 01:49:44 +0100

    feature: add AuthProvider

--- src/common/context/AuthProvider.tsx
+++ src/common/context/AuthProvider.tsx
@@ -1,9 +1,31 @@
-import { createContext, useContext } from 'react';
-const AuthContext = createContext('null');
+import { createContext, useContext, useState } from 'react';
+import { Api } from '../base/Api';
+import { Auth } from '../models/Auth';
+import { User } from '../models/User';
+
+const AuthContext = createContext<Auth>({} as Auth);
 export const useAuth = () => useContext(AuthContext);
 
 const AuthProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
-  const data = 'fujara';
+  const [user, setUser] = useState<User | null>(null);
+  const [error, setError] = useState('');
+
+  const getCurrentUser = async () => {
+    try {
+      console.log('fetching');
+      const response = await Api.get('/currentUser');
+      console.log(response);
+    } catch (error: any) {
+      console.log(error.response.data);
+      setError('Cannot get user');
+    }
+  };
+
+  const data = {
+    getCurrentUser,
+    user,
+    error,
+  };
   return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>;
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import AuthProvider from './common/context/AuthProvider';
 import { router } from './features/routes/router';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>
 );
