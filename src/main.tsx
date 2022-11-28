198a7e1da8f57d56e5138d0bbc53156282313226
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-11-28 01:04:26 +0100

    change: move file

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './common/base/router';
+import { router } from './features/routes/router';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
