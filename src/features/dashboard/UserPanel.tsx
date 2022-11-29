5da5ced3d1acea1dc1f9bd1e40178916145a2044
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-11-29 12:57:00 +0100

    feature: add initial user panel

--- src/features/dashboard/UserPanel.tsx
+++ src/features/dashboard/UserPanel.tsx
@@ -1,10 +1,23 @@
-import { useEffect } from 'react';
-import { Api } from '../../common/base/Api';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../common/context/AuthProvider';
 
-type Props = {};
+const UserPanel = () => {
+  const { logout, user } = useAuth();
+  const navigate = useNavigate();
 
-const UserPanel = (props: Props) => {
-  return <div>UserPanel</div>;
+  const handleLogout = async () => {
+    await logout();
+    navigate('/signin');
+  };
+
+  return (
+    <div>
+      <p>id: {user?.id}</p>
+      <p>email: {user?.email}</p>
+      <p>name: {user?.name}</p>
+      <button onClick={handleLogout}>logout</button>
+    </div>
+  );
 };
 
 export default UserPanel;
