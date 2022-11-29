4406844be560bc19c8e24aa6ec7e91bdd1ebde6f
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-11-29 14:31:46 +0100

    feature: auth user

--- src/features/routes/PrivateRoute.tsx
+++ src/features/routes/PrivateRoute.tsx
@@ -1,11 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet, Navigate, RouteProps } from 'react-router-dom';
 import { useAuth } from '../../common/context/AuthProvider';
 
 const PrivateRoute: React.FC<RouteProps> = () => {
-  const { user } = useAuth();
+  const { isAuthenticated } = useAuth();
 
-  return user ? <Outlet /> : <Navigate to='/signin' />;
+  return isAuthenticated ? <Outlet /> : <Navigate to='/signin' />;
 };
 
 export default PrivateRoute;
