6ab6867a338cadfed1749c36467b0771b0aef442
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-11-29 13:01:16 +0100

    feature: add route

--- src/features/routes/router.tsx
+++ src/features/routes/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Page404 from '../../components/404/Page404';
 import UserPanel from '../dashboard/UserPanel';
+import SignIn from '../signin/Signin';
 import SignUp from '../signup/SignUp';
 import PrivateRoute from './PrivateRoute';
 
@@ -8,6 +9,10 @@ export const router = createBrowserRouter([
   {
     path: '/signup',
     element: <SignUp />,
+  },
+  {
+    path: '/signin',
+    element: <SignIn />,
   },
   {
     path: '/',
