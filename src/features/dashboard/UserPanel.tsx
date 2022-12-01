925e32ff9749ddc273e9441960fd2814ec5f208e
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-12-01 21:59:31 +0100

    fix: small fix

--- src/features/dashboard/UserPanel.tsx
+++ src/features/dashboard/UserPanel.tsx
@@ -4,26 +4,27 @@ import { useAuth } from '../../common/context/AuthProvider';
 import UserProvider, { useUser } from '../../common/context/UserProvider';
 
 const UserPanel = () => {
-  const { logout } = useAuth();
+  const { logout, isLoading, isAuthenticated } = useAuth();
   const { user, fetchCurrentUser } = useUser();
   const navigate = useNavigate();
 
   const handleLogout = async () => {
     await logout();
-    navigate('/signin');
   };
 
   useEffect(() => {
     (async () => await fetchCurrentUser())();
   }, []);
 
-  return (
-    <UserProvider>
-      <p>id: {user?.id}</p>
-      <p>email: {user?.email}</p>
-      <p>name: {user?.name}</p>
+  return isLoading ? (
+    <div>Loading...</div>
+  ) : (
+    <div>
+      <p>id: {user ? user.id : 'Loading...'}</p>
+      <p>email: {user ? user.email : 'Loading...'}</p>
+      <p>name: {user ? user.name : 'Loading...'}</p>
       <button onClick={handleLogout}>logout</button>
-    </UserProvider>
+    </div>
   );
 };
 
