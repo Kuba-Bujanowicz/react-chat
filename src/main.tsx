fe3009e297babf7d026b3105ebd9bf8b665f4ad7
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-12-01 21:57:19 +0100

    feature: apply user provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import AuthProvider from './common/context/AuthProvider';
+import UserProvider from './common/context/UserProvider';
 import { router } from './features/routes/router';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
@@ -9,7 +10,9 @@ const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <UserProvider>
+        <RouterProvider router={router} />
+      </UserProvider>
     </AuthProvider>
   </StrictMode>
 );
