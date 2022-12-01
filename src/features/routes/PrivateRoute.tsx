54ceee33187398e0329a27a491281adc012ceac7
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-12-01 22:00:34 +0100

    feature: take care of authentication

--- src/features/routes/PrivateRoute.tsx
+++ src/features/routes/PrivateRoute.tsx
@@ -1,11 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Outlet, Navigate, RouteProps } from 'react-router-dom';
 import { useAuth } from '../../common/context/AuthProvider';
 
 const PrivateRoute: React.FC<RouteProps> = () => {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, isAuthenticating, checkAuth } = useAuth();
 
-  return isAuthenticated ? <Outlet /> : <Navigate to='/signin' />;
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  return isAuthenticating ? <div>Veryfing...</div> : isAuthenticated ? <Outlet /> : <Navigate to='/signin' />;
 };
 
 export default PrivateRoute;
