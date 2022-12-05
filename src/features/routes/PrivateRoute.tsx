3f4e42ba0951976773b7bc2c42f09ad15118487f
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-12-05 04:20:22 +0100

    feature: add separate flag for authentication

--- src/features/routes/PrivateRoute.tsx
+++ src/features/routes/PrivateRoute.tsx
@@ -2,9 +2,9 @@ import { Outlet, Navigate, RouteProps } from 'react-router-dom';
 import { useAuth } from '../../common/context/AuthProvider';
 
 const PrivateRoute: React.FC<RouteProps> = () => {
-  const { isAuthenticated, isLoading } = useAuth();
+  const { isAuthenticated, isAuthenticating } = useAuth();
 
-  return isLoading ? <div>Veryfing...</div> : isAuthenticated ? <Outlet /> : <Navigate to='/signin' />;
+  return isAuthenticating ? <div>Veryfing...</div> : isAuthenticated ? <Outlet /> : <Navigate to='/signin' />;
 };
 
 export default PrivateRoute;
