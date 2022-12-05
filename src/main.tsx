92f496b278ed60e982e9143d733f8febd89caf24
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-12-05 04:18:56 +0100

    feature: set height and remove margins

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import AuthProvider from './common/context/AuthProvider';
-import UserProvider from './common/context/UserProvider';
+import { Loading } from './components/hoc/Loading';
 import { router } from './features/routes/router';
+import './global.scss';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <StrictMode>
     <AuthProvider>
-      <UserProvider>
+      <Loading>
         <RouterProvider router={router} />
-      </UserProvider>
+      </Loading>
     </AuthProvider>
   </StrictMode>
 );
