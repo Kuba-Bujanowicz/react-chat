c427f8c65dd9d99f3bf16e9def81b5abc7a853d7
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-12-05 04:22:09 +0100

    change: simplify with hook

--- src/features/dashboard/UserPanel.tsx
+++ src/features/dashboard/UserPanel.tsx
@@ -1,25 +1,16 @@
-import { useEffect } from 'react';
 import { useAuth } from '../../common/context/AuthProvider';
-import { useUser } from '../../common/context/UserProvider';
+import useUser from './useUser';
 
 const UserPanel = () => {
   const { logout } = useAuth();
-  const { user, fetchCurrentUser } = useUser();
-
-  const handleLogout = async () => {
-    await logout();
-  };
-
-  useEffect(() => {
-    (async () => await fetchCurrentUser())();
-  }, []);
+  const { user } = useUser();
 
   return (
     <div>
       <p>id: {user ? user.id : 'Loading...'}</p>
       <p>email: {user ? user.email : 'Loading...'}</p>
       <p>name: {user ? user.name : 'Loading...'}</p>
-      <button onClick={handleLogout}>logout</button>
+      <button onClick={logout}>logout</button>
     </div>
   );
 };
