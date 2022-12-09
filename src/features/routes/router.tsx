9a37a05adeae9deb00deefdb9a758bc52a352af5
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-12-09 22:39:41 +0100

    feature: add email page

--- src/features/routes/router.tsx
+++ src/features/routes/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Page404 from '../../components/404/Page404';
 import UserPanel from '../dashboard/UserPanel';
+import EmailVerification from '../emailVerification/EmailVerification';
 import SignIn from '../signin/SignIn';
 import SignUp from '../signup/SignUp';
 import PrivateRoute from './PrivateRoute';
@@ -13,6 +14,10 @@ export const router = createBrowserRouter([
   {
     path: '/signin',
     element: <SignIn />,
+  },
+  {
+    path: '/verify-email',
+    element: <EmailVerification />,
   },
   {
     path: '/',
