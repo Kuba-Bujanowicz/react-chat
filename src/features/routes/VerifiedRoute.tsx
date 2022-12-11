1e3824d12af9c258368785e1be12e9e866ea53ae
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-12-11 22:12:04 +0100

    Fix returning outlet instead of redirecting

--- src/features/routes/VerifiedRoute.tsx
+++ src/features/routes/VerifiedRoute.tsx
@@ -4,7 +4,7 @@ import { useUser } from '../../common/context/UserProvider';
 const VerifiedRoute: React.FC<RouteProps> = () => {
   const { user, isLoading } = useUser();
 
-  return isLoading ? null : user ? user.isVerified ? <Navigate to='/' /> : <Navigate to='/verify-email' /> : <Navigate to='/signin' replace />;
+  return isLoading ? null : user ? user.isVerified ? <Outlet /> : <Navigate to='/verify-email' /> : <Navigate to='/signin' replace />;
 };
 
 export default VerifiedRoute;
