1301e896f144fcffb571a7dc53ef84fb1279a3d7
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-12-11 22:11:14 +0100

    Check if authenticated

--- src/features/routes/PersistRedirectRoute.tsx
+++ src/features/routes/PersistRedirectRoute.tsx
@@ -1,11 +1,13 @@
 import { ReactElement } from 'react';
 import { Navigate, RouteProps } from 'react-router-dom';
+import { useAuth } from '../../common/context/AuthProvider';
 import { useUser } from '../../common/context/UserProvider';
 
 const PersistRedirectRoute: React.FC<RouteProps> = ({ element }) => {
   const { user, isLoading } = useUser();
+  const { isAuthenticated, isAuthenticating } = useAuth();
 
-  return isLoading ? null : user ? <Navigate to='/' /> : (element as ReactElement);
+  return isLoading || isAuthenticating ? null : user && isAuthenticated ? <Navigate to='/' /> : (element as ReactElement);
 };
 
 export default PersistRedirectRoute;
