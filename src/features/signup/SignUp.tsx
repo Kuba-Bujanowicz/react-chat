682912684f71139f56df92057ea9a31a5d32aecb
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-12-11 23:41:54 +0100

    Create step-by-step form

--- src/features/signup/SignUp.tsx
+++ src/features/signup/SignUp.tsx
@@ -1,11 +1,22 @@
+import SignUpEmailStep from './SignUpEmailStep';
 import SignUpForm from './SignUpForm';
+import { FormHelper } from '../../common/helpers/FormHelper';
+import { SignUpData } from '../../common/models/Auth';
+import { useState } from 'react';
+import SignUpUsernameStep from './SignUpUsernameStep';
 
 const SignUp = () => {
+  const [state, setState] = useState<SignUpData>({} as SignUpData);
+  const binder = new FormHelper<SignUpData>(setState);
+  const [step, setStep] = useState(0);
+
   return (
     <main className='signup'>
       <h1 className='signup__title'>Signup</h1>
       <div className='signup__form box'>
-        <SignUpForm />
+        {/* <SignUpForm /> */}
+        <SignUpEmailStep binder={binder} canMount={step >= 0} state={state} handleNextStep={setStep.bind(this, (prevState) => prevState + 1)} />
+        <SignUpUsernameStep binder={binder} canMount={step >= 1} state={state} handleNextStep={setStep.bind(this, (prevState) => prevState + 1)} />
       </div>
       <a href='/signin'>Already signed up?</a>
     </main>
