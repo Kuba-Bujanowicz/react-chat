a69850caab014e915fa5620df6c16bbdd66d885e
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-12-11 23:40:47 +0100

    Use spinner instead of plain text

--- src/components/hoc/Loader.tsx
+++ src/components/hoc/Loader.tsx
@@ -1,16 +1,13 @@
 import { PropsWithChildren } from 'react';
 import { useAuth } from '../../common/context/AuthProvider';
 import { useUser } from '../../common/context/UserProvider';
+import Spinner from '../spinner/Spinner';
 
 const Loader: React.FC<PropsWithChildren> = ({ children }) => {
   const { isLoading } = useUser();
   const { isAuthenticating } = useAuth();
 
-  return isLoading || isAuthenticating ? (
-    <div style={{ height: '100vh', width: '100%', textAlign: 'center', lineHeight: '100vh', color: 'white', backgroundColor: 'rgba(0,0,0,0.7)' }}>Loading...</div>
-  ) : (
-    <>{children}</>
-  );
+  return isLoading || isAuthenticating ? <Spinner /> : <>{children}</>;
 };
 
 export default Loader;
