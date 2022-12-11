03277ae23f15fbbda4092dd59854990a61ff4b22
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-12-11 03:25:14 +0100

    feature: add user provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import AuthProvider from './common/context/AuthProvider';
-import { Loading } from './components/hoc/Loading';
+import UserProvider from './common/context/UserProvider';
 import { router } from './features/routes/router';
 import './global.scss';
 
@@ -11,9 +11,9 @@ const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <StrictMode>
     <AuthProvider>
-      <Loading>
+      <UserProvider>
         <RouterProvider router={router} />
-      </Loading>
+      </UserProvider>
     </AuthProvider>
   </StrictMode>
 );
