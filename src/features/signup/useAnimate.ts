85355cef63b79f0f3dbb4aa817e75b05f022f7e3
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-12-23 07:05:41 +0100

    Rename, add transitions and better exit animation

--- src/features/signup/useAnimate.ts
+++ src/features/signup/useAnimate.ts
@@ -1,10 +1,10 @@
 export const useAnimate = () => {
   return {
-    slide(dir: 'up' | 'down' | 'left' | 'right', duration: number = 0.5, delay: number = 0) {
+    slideAndFade(sDir: 'up' | 'down' | 'left' | 'right', fDir: 'out' | 'in', duration: number = 0.5, delay: number = 0) {
       return {
-        initial: { [dir === 'up' || dir === 'down' ? 'y' : 'x']: dir === 'up' ? 100 : dir === 'down' ? -100 : dir === 'left' ? 100 : -100 },
-        animate: { [dir === 'up' || dir === 'down' ? 'y' : 'x']: 0 },
-        exit: { [dir === 'up' || dir === 'down' ? 'y' : 'x']: dir === 'up' ? -100 : dir === 'down' ? 100 : dir === 'left' ? -100 : 100 },
+        initial: { [sDir === 'up' || sDir === 'down' ? 'y' : 'x']: sDir === 'up' ? 50 : sDir === 'down' ? -50 : sDir === 'left' ? 50 : -50, opacity: fDir === 'out' ? 1 : 0 },
+        animate: { [sDir === 'up' || sDir === 'down' ? 'y' : 'x']: 0, opacity: fDir === 'out' ? 0 : 1 },
+        exit: { [sDir === 'up' || sDir === 'down' ? 'y' : 'x']: sDir === 'up' ? -50 : sDir === 'down' ? 50 : sDir === 'left' ? -50 : 50, opacity: fDir === 'out' ? 0 : 1 },
         transition: { duration, delay },
       };
     },
@@ -13,7 +13,7 @@ export const useAnimate = () => {
       return {
         initial: { opacity: dir === 'out' ? 1 : 0 },
         animate: { opacity: dir === 'out' ? 0 : 1 },
-        exit: { opacity: dir === 'out' ? 0 : 1 },
+        exit: { opacity: 0  },
         transition: { duration, delay },
       };
     },
@@ -21,7 +21,7 @@ export const useAnimate = () => {
     fadeInOut(duration: number = 0.5, delay: number = 0) {
       return {
         initial: { opacity: 0 },
-        animate: { opacity: 1 },
+        animate: { opacity: 1, transition: { duration, delay } },
         exit: { opacity: 0 },
         transition: { duration, delay },
       };
