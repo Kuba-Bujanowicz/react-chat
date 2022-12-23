0404025dd79c3d631a0014f4b699cf94e371fc18
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-12-23 07:04:28 +0100

    Split welcome text and form into two components

--- src/features/signup/SignUp.tsx
+++ src/features/signup/SignUp.tsx
@@ -1,28 +1,20 @@
-import SignUpEmailStep from './SignUpEmailStep';
-import { FormHelper } from '../../common/helpers/FormHelper';
-import { SignUpData } from '../../common/models/Auth';
-import { useState } from 'react';
-import SignUpUsernameStep from './SignUpUsernameStep';
-import { AnimatePresence, motion } from 'framer-motion';
-import Welcome from './Welcome';
+import { useState } from "react";
+import { motion } from "framer-motion";
+import Welcome from "./Welcome";
+import SignUpForm from "./SignUpForm";
 
 const SignUp = () => {
-  const [state, setState] = useState<SignUpData>({} as SignUpData);
-  const binder = new FormHelper<SignUpData>(setState);
-  const [currentStep, setCurrentStep] = useState(0);
+  const [isFormVisible, setIsFormVisible] = useState(false);
 
-  const handleNextStep = () => {
-    setCurrentStep((prevState) => prevState + 1);
+  const handleWelcomeAnimationComplete = () => {
+    setIsFormVisible(true);
   };
 
   return (
-    <motion.main className='signup'>
-      <AnimatePresence>
-        {currentStep === 0 ? <Welcome key='step-0' handleNextStep={handleNextStep} /> : null}
-        {currentStep > 0 && <SignUpEmailStep key='step-1' binder={binder} state={state} handleNextStep={handleNextStep} />}
-        {currentStep > 1 && <SignUpUsernameStep key='step-2' binder={binder} state={state} handleNextStep={handleNextStep} />}
-      </AnimatePresence>
-    </motion.main>
+    <main className="signup">
+      <Welcome onAnimationComplete={handleWelcomeAnimationComplete} />
+      {isFormVisible && <SignUpForm />}
+    </main>
   );
 };
 
