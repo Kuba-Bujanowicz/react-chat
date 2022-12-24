3301ad71eb9b44c90ad6f2d0ae77627537be41ff
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-12-24 04:41:31 +0100

    Create input component

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,19 +1,44 @@
-import { InputHTMLAttributes } from 'react';
-import Spinner from '../spinner/Spinner';
+import { InputHTMLAttributes } from "react";
+import { ReactSVG } from "react-svg";
+import Spinner from "../spinner/Spinner";
+import validIcon from "../../assets/icons/valid.svg";
+import invalidIcon from "../../assets/icons/invalid.svg";
 
 type Props = {
-  isLoading: boolean;
+  inputName: string;
+  displayType?: "error" | "success" | "loading";
+  error?: string;
+  icon?: string;
 };
 
-const Input: React.FC<InputHTMLAttributes<HTMLInputElement> & Props> = ({ isLoading, ...rest }) => {
+const Input: React.FC<InputHTMLAttributes<HTMLInputElement> & Props> = ({
+  inputName,
+  displayType,
+  error,
+  icon,
+  ...rest
+}) => {
+  const getIcon = () => {
+    switch (displayType) {
+      case "error":
+        return <ReactSVG src={invalidIcon} />;
+      case "success":
+        return <ReactSVG src={validIcon} />;
+      case "loading":
+        return <Spinner />;
+      default:
+        return icon ? <ReactSVG src={icon} /> : null;
+    }
+  };
+
   return (
-    <div className='input'>
-      <input {...rest} />
-      {isLoading && (
-        <div className='input__spinner'>
-          <Spinner />
-        </div>
-      )}
+    <div className={`input ${error ? "input--error" : ""}`}>
+      <div className="input__ctn">
+        <input className="input__field" required {...rest} />
+        <label className="input__label">{inputName}</label>
+        <div className="input__icon">{getIcon()}</div>
+      </div>
+      <p className="input__error">{error}</p>
     </div>
   );
 };
