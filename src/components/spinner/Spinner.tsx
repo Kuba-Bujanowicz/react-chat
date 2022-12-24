e11d3593911e9841a8030bbf6727c5dbbe7c815e
Kuba Bujanowicz <bujanowicz_kuba@interia.pl>
2022-12-24 04:42:07 +0100

    Add other spinner icon

--- src/components/spinner/Spinner.tsx
+++ src/components/spinner/Spinner.tsx
@@ -1,11 +1,5 @@
 const Spinner = () => {
-  return (
-    <div className='spinner'>
-      <span className='spinner-inner-1'></span>
-      <span className='spinner-inner-2'></span>
-      <span className='spinner-inner-3'></span>
-    </div>
-  );
+  return <div className="spinner"></div>;
 };
 
 export default Spinner;
